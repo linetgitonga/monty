83da8575db6d7ffa6e22e262faba535e6882a4cc
linetgitonga <linetgitonga55@gmail.com>
2023-12-18 17:17:52 -0800

    chang

--- main.c
+++ main.c
@@ -4,6 +4,7 @@ bus_t bus = {NULL, NULL, NULL, 0};
 * main - monty code interpreter
 * @argc: number of arguments
 * @argv: monty file location
+* Author :Linet Gitonga
 * Return: 0 on success
 */
 int main(int argc, char *argv[])
